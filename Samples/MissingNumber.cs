29de01202fbac127c89b68832d21ddaf5b2bbc5c
rahulhdez <77864943+rahulhdez@users.noreply.github.com>
2023-12-14 12:25:18 -0600

    second commit

---
+++ Samples/MissingNumber.cs
@@ -0,0 +1,22 @@
+﻿
+// int lim_min = 10;
+// int lim_max = 20;
+int[] nums =  { 10, 11, 12, 13, 15, 16, 17, 19, 20};
+
+int missing = 0;
+
+int i = 10;
+foreach (var num in nums)
+{
+    if( num != i )
+        {
+            missing = i;
+            break;
+        }
+    i++;
+}
+
+if(i < 20)
+Console.WriteLine($"The missing number is: {missing}");
+else
+Console.WriteLine("There is no missing number");
